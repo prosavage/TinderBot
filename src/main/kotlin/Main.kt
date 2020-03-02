60be3bdc75627af975eeeeb5cd37ceb1c789f36f
prosavage <naman0311@gmail.com>
2020-03-02 15:18:44 -0600

    works lmao

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,17 @@
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
+import org.json.JSONArray
+import org.openqa.selenium.By
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import java.io.File
+import java.lang.StringBuilder
+import java.util.*
 
 fun main(args: Array<String>) {
     Main().enable()
 }
-
 
 
 class Main() {
@@ -13,9 +20,31 @@ class Main() {
         val options = ChromeOptions()
         options.addArguments("user-data-dir=~/Documents/data")
         val driver = ChromeDriver(options)
-        driver.get("https://tinder.com")
+        driver.get("https://tinder.com/app/recs")
+        Thread.sleep(4000)
+        println("READY!")
+        val box = driver.findElements(
+            By.xpath(
+                "/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[1]/div[3]/div[1]/div[1]/div/div[1]/div"
+            )
+        )
+        val url = box.first().findElement(By.tagName("div")).getCssValue("background-image")
+        if (url == "none") {
+            println("Something went wrong, maybe wrong element was selected.")
+            return
+        }
+        val processedUrl = url.replace("url(\"", "").replace("\")", "")
+        val fileUUID = UUID.randomUUID().toString()
+        val fileToSave = File("/Users/prosavage/Documents/data", "$fileUUID.webp").toPath().toString()
+        saveImage(processedUrl, fileToSave)
+        Thread.sleep(1000)
+        val response = sendReq(fileToSave)
+        println(response)
+//        val parser = Parser()
+//        val json = parser.parse(StringBuilder(response)) as JsonObject
+//        val people = json["people"] as JsonArray<JsonObject>
+//        println(people[0]["attractiveness"])
     }
-
 
 
 }
--- src/main/kotlin/Util.kt
+++ src/main/kotlin/Util.kt
@@ -1,12 +1,14 @@
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.support.ui.WebDriverWait
-import java.io.FileOutputStream
-import java.io.IOException
+import java.io.*
 import java.net.URL
+import java.net.URLConnection
 import java.nio.channels.Channels
 import java.nio.channels.FileChannel
 import java.nio.channels.ReadableByteChannel
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 fun waitUntilPageIsReady(driver: ChromeDriver) {
@@ -20,4 +22,29 @@ fun saveImage(imageUrl: String?, destinationFile: String?) {
     val fileOutputStream = FileOutputStream(destinationFile)
     val fileChannel: FileChannel = fileOutputStream.channel
     fileOutputStream.channel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE)
+}
+
+fun sendReq(filePath: String): String {
+    val url =
+        URL("https://api.haystack.ai/api/image/analyze?apikey=bcc555e7f0a8f53f04f3f2abeae71611&output=json&model=attractiveness")
+    val conn: URLConnection = url.openConnection()
+    conn.setDoOutput(true)
+
+    val imageData: ByteArray = Files.readAllBytes(Paths.get(filePath))
+    val os: OutputStream = BufferedOutputStream(conn.getOutputStream())
+    os.write(imageData)
+    os.close()
+
+    val `is`: InputStream = conn.getInputStream()
+    var buffer = ByteArray(1024)
+    val responseBuffer = ByteArrayOutputStream()
+
+    while (true) {
+        val n: Int = `is`.read(buffer, 0, buffer.size)
+        if (n <= 0) break
+        responseBuffer.write(buffer, 0, n)
+    }
+
+    val response: String = responseBuffer.toString("UTF-8")
+    return response
 }
